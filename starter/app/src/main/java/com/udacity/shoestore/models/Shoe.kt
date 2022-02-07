ba5fb095247780e5598915b9facc57bd5c0f7e24
xaviermward <xavier.ward3289@outlook.com>
2022-02-06 23:48:43 -0500

    Migrated from kotlin-android-extensions and added proper parcelize plugIN. Updated xml layouts and color and string values

--- starter/app/src/main/java/com/udacity/shoestore/models/Shoe.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/Shoe.kt
@@ -1,7 +1,8 @@
 package com.udacity.shoestore.models
 
+import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+
 
 @Parcelize
 data class Shoe(var name: String, var size: Double, var company: String, var description: String,
